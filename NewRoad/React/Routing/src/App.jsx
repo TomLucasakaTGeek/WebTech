67741f91cfac13dcdf62092b410df3ae42c70ae9
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2024-03-31 15:13:37 +0530

    Done with basics of Routing

--- NewRoad/React/Routing/src/App.jsx
+++ NewRoad/React/Routing/src/App.jsx
@@ -2,9 +2,30 @@ import './App.css'
 import { Signup } from './Pages/Signup Page/Signup.jsx'
 import { Login } from './Pages/Login Page/Login.jsx'
 import { Message } from './Pages/Messaging Page/Message.jsx'
+import Navbar from './Pages/Navbar/navbar.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {  
-  return <Login/>
+    const router = createBrowserRouter([
+        {
+          path: '/login',
+          element: <><Login/></>
+        },
+        {
+          path:'/signup',
+          element: <><Signup/></>
+        },
+        {
+          path: '/chat',
+          element: <><Message/></>
+        }
+    ])
+
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
 }
 
 export default App
