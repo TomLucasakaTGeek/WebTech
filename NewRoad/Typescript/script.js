82d7bbf2d2613a2d9f9e8cd1033f4fcea23d4633
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2024-08-16 19:42:02 +0530

    union & intersection types

--- NewRoad/Typescript/script.js
+++ NewRoad/Typescript/script.js
@@ -1,10 +1,7 @@
 for (var i = 0; i <= 5; i++)
     console.log(5);
-//enum types
-var Directions;
-(function (Directions) {
-    Directions["up"] = "UP";
-    Directions["down"] = "DOWN";
-    Directions["left"] = "LEFT";
-    Directions["right"] = "RIGHT";
-})(Directions || (Directions = {}));
+//optional types
+let variable;
+variable = null;
+variable = '12';
+variable = 12;
--- NewRoad/Typescript/script.ts
+++ NewRoad/Typescript/script.ts
@@ -2,10 +2,33 @@ for(let i : number = 0; i <= 5; i++)
     console.log(5)
 
 //optional types
-let variable: string | null;
 
-variable = null;
-variable = '12';
-variable = 12;
+//union type
+let v: string | number;
+v = "12";
+
+function abcd(variable: string | number) {
+    if(typeof variable === 'number')
+        variable.toFixed(2)
+    else if(typeof variable === 'string')
+        variable.toUpperCase()
+}
+
+abcd(12);
+
+//intersection type
+type classmates = {
+    section: string
+}
+type students = {
+    name: string
+}
+type ClassmatesInStudents = classmates & students;
+
+let a: ClassmatesInStudents = {
+    name: "Harsh",
+    section: "A"
+}
+
 
 
