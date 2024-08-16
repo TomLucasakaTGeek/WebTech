ec89e4b9b13ae8b222f60796c2a6f9e2f2f1d242
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2024-08-16 19:16:17 +0530

    enum types

---
+++ NewRoad/Typescript/script.js
@@ -0,0 +1,10 @@
+for (var i = 0; i <= 5; i++)
+    console.log(5);
+//enum types
+var Directions;
+(function (Directions) {
+    Directions["up"] = "UP";
+    Directions["down"] = "DOWN";
+    Directions["left"] = "LEFT";
+    Directions["right"] = "RIGHT";
+})(Directions || (Directions = {}));
